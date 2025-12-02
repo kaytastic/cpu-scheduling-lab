3c83c57fbf386ea5e8cea938b3e4d2fa19aa40ed
kaytastic <ktg8548@nyu.edu>
2025-12-02 16:24:06 -0500

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -6,16 +6,16 @@ struct PCB handle_process_arrival_pp(struct PCB *ready_queue, int *queue_cnt,
                                      struct PCB current_process,
                                      struct PCB new_process, int time_stamp)
 {
-    if (new_process.process_priority < current_process.process_priority) {
-        current_process.remaining_bursttime -= time_stamp - current_process.execution_starttime;
-        current_process.execution_starttime = 0;
-        current_process.execution_endtime = 0;
-        ready_queue[*queue_cnt] = current_process;
-        (*queue_cnt)++;
+    if (current_process.process_id == 0) {
+        new_process.execution_starttime = time_stamp;
+        new_process.execution_endtime = time_stamp + new_process.total_bursttime;
+        return new_process;
     }
 
     if (new_process.process_priority < current_process.process_priority) {
         current_process.remaining_bursttime -= time_stamp - current_process.execution_starttime;
+        current_process.execution_starttime = 0;
+        current_process.execution_endtime = 0;
         ready_queue[*queue_cnt] = current_process;
         (*queue_cnt)++;
 
@@ -24,6 +24,9 @@ struct PCB handle_process_arrival_pp(struct PCB *ready_queue, int *queue_cnt,
         return new_process;
     }
 
+    new_process.execution_starttime = 0;
+    new_process.execution_endtime = 0;
+    new_process.remaining_bursttime = new_process.total_bursttime;
     ready_queue[*queue_cnt] = new_process;
     (*queue_cnt)++;
     return current_process;
@@ -69,19 +72,23 @@ struct PCB handle_process_arrival_srtp(struct PCB *ready_queue, int *queue_cnt,
 
     int executed = time_stamp - current_process.execution_starttime;
     int current_rbt = current_process.remaining_bursttime - executed;
-  
-  if (new_process.remaining_bursttime < current_rbt) {
-      current_process.remaining_bursttime = current_rbt;
-      current_process.execution_starttime = 0;
-      current_process.execution_endtime = 0;
-      ready_queue[*queue_cnt] = current_process;
-      (*queue_cnt)++;
 
-      new_process.execution_starttime = time_stamp;
-      new_process.execution_endtime = time_stamp + new_process.total_bursttime;
-      return new_process;
+    if (new_process.remaining_bursttime < current_rbt) {
+        // preempt current process
+        current_process.remaining_bursttime = current_rbt;
+        current_process.execution_starttime = 0;
+        current_process.execution_endtime = 0;
+        ready_queue[*queue_cnt] = current_process;
+        (*queue_cnt)++;
+
+        new_process.execution_starttime = time_stamp;
+        new_process.execution_endtime = time_stamp + new_process.total_bursttime;
+        return new_process;
     }
 
+    new_process.execution_starttime = 0;
+    new_process.execution_endtime = 0;
+    new_process.remaining_bursttime = new_process.total_bursttime;
     ready_queue[*queue_cnt] = new_process;
     (*queue_cnt)++;
     return current_process;
@@ -140,20 +147,28 @@ struct PCB handle_process_completion_rr(struct PCB *ready_queue, int *queue_cnt,
         struct PCB nullpcb = {0,0,0,0,0,0,0};
         return nullpcb;
     }
-  int idx = 0;
-  for (int i = 1; i < *queue_cnt; i++) {
-    if (ready_queue[i].arrival_timestamp < ready_queue[idx].arrival_timestamp)
-      idx = i;
+
+    // Find process with earliest arrival time (FIFO)
+    int idx = 0;
+    for (int i = 1; i < *queue_cnt; i++) {
+        if (ready_queue[i].arrival_timestamp < ready_queue[idx].arrival_timestamp)
+            idx = i;
     }
 
-    struct PCB chosen = ready_queue[0];
+    struct PCB chosen = ready_queue[idx];
 
-    for (int i = 0; i < *queue_cnt - 1; i++)
+    // Shift elements to remove the chosen process
+    for (int i = idx; i < *queue_cnt - 1; i++)
         ready_queue[i] = ready_queue[i+1];
 
     (*queue_cnt)--;
 
     chosen.execution_starttime = time_stamp;
-    chosen.execution_endtime = time_stamp + chosen.remaining_bursttime;
+    // Use minimum of time_quantum and remaining_bursttime
+    if (chosen.remaining_bursttime < time_quantum)
+        chosen.execution_endtime = time_stamp + chosen.remaining_bursttime;
+    else
+        chosen.execution_endtime = time_stamp + time_quantum;
+    
     return chosen;
 }
