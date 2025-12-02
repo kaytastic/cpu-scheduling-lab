99e32f94f95bcbe3c07085a91ce965a015764b35
kaytastic <ktg8548@nyu.edu>
2025-12-02 15:20:02 -0500

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -6,19 +6,19 @@ struct PCB handle_process_arrival_pp(struct PCB *ready_queue, int *queue_cnt,
                                      struct PCB current_process,
                                      struct PCB new_process, int time_stamp)
 {
-    if (current_process.PID == 0) {
-        new_process.EST = time_stamp;
-        new_process.EET = time_stamp + new_process.TBT;
+    if (current_process.process_id == 0) {
+        new_process.execution_starttime = time_stamp;
+        new_process.execution_starttime = time_stamp + new_process.total_bursttime;
         return new_process;
     }
 
-    if (new_process.Priority < current_process.Priority) {
-        current_process.RBT -= time_stamp - current_process.EST;
+    if (new_process.process_priority < current_process.Priority) {
+        current_process.remaining_bursttime -= time_stamp - current_process.execution_starttime;
         ready_queue[*queue_cnt] = current_process;
         (*queue_cnt)++;
 
-        new_process.EST = time_stamp;
-        new_process.EET = time_stamp + new_process.TBT;
+        new_process.execution_starttime = time_stamp;
+        new_process.execution_endtime = time_stamp + new_process.total_bursttime;
         return new_process;
     }
 
@@ -37,7 +37,7 @@ struct PCB handle_process_completion_pp(struct PCB *ready_queue, int *queue_cnt,
 
     int idx = 0;
     for (int i = 1; i < *queue_cnt; i++) {
-        if (ready_queue[i].Priority < ready_queue[idx].Priority)
+        if (ready_queue[i].process_priority < ready_queue[idx].Priority)
             idx = i;
     }
 
@@ -48,8 +48,8 @@ struct PCB handle_process_completion_pp(struct PCB *ready_queue, int *queue_cnt,
 
     (*queue_cnt)--;
 
-    chosen.EST = time_stamp;
-    chosen.EET = time_stamp + chosen.TBT;
+    chosen.execution_starttime = time_stamp;
+    chosen.execution_endtime = time_stamp + chosen.total_bursttime;
     return chosen;
 }
 
@@ -60,22 +60,22 @@ struct PCB handle_process_arrival_srtp(struct PCB *ready_queue, int *queue_cnt,
                                        struct PCB new_process, int time_stamp)
 {
     if (current_process.PID == 0) {
-        new_process.EST = time_stamp;
-        new_process.EET = time_stamp + new_process.TBT;
+        new_process.execution_starttime = time_stamp;
+        new_process.execution_endtime = time_stamp + new_process.total_bursttime;
         return new_process;
     }
 
-    int executed = time_stamp - current_process.EST;
-    int current_rbt = current_process.RBT - executed;
+    int executed = time_stamp - current_process.execution_starttime;
+    int current_rbt = current_process.remaining_bursttime - executed;
 
-    if (new_process.RBT < current_rbt) {
+    if (new_process.remaining_bursttime < current_rbt) {
         // preempt current process
-        current_process.RBT = current_rbt;
+        current_process.remaining_bursttime = current_rbt;
         ready_queue[*queue_cnt] = current_process;
         (*queue_cnt)++;
 
-        new_process.EST = time_stamp;
-        new_process.EET = time_stamp + new_process.TBT;
+        new_process.execution_starttime = time_stamp;
+        new_process.execution_endtime = time_stamp + new_process.total_bursttime;
         return new_process;
     }
 
@@ -95,7 +95,7 @@ struct PCB handle_process_completion_srtp(struct PCB *ready_queue, int *queue_cn
     // select smallest RBT
     int idx = 0;
     for (int i = 1; i < *queue_cnt; i++) {
-        if (ready_queue[i].RBT < ready_queue[idx].RBT)
+        if (ready_queue[i].remaining_bursttime < ready_queue[idx].remaining_bursttime)
             idx = i;
     }
 
@@ -106,8 +106,8 @@ struct PCB handle_process_completion_srtp(struct PCB *ready_queue, int *queue_cn
 
     (*queue_cnt)--;
 
-    chosen.EST = time_stamp;
-    chosen.EET = time_stamp + chosen.RBT;
+    chosen.execution_starttime = time_stamp;
+    chosen.execution_endtime = time_stamp + chosen.remaining_bursttime;
     return chosen;
 }
 
@@ -118,9 +118,9 @@ struct PCB handle_process_arrival_rr(struct PCB *ready_queue, int *queue_cnt,
                                         struct PCB new_process,
                                         int time_stamp, int time_quantum)
 {
-    if (current_process.PID == 0) {
-      new_process.EST = time_stamp;
-      new_process.EET = time_stamp + new_process.TBT; 
+    if (current_process.process_id == 0) {
+      new_process.execution_starttime = time_stamp;
+      new_process.execution_endtime = time_stamp + new_process.total_bursttime; 
         return new_process;
     }
 
@@ -145,7 +145,7 @@ struct PCB handle_process_completion_rr(struct PCB *ready_queue, int *queue_cnt,
 
     (*queue_cnt)--;
 
-    chosen.EST = time_stamp;
-    chosen.EET = time_stamp + chosen.RBT;
+    chosen.execution_starttime = time_stamp;
+    chosen.execution_endtime = time_stamp + chosen.remaining_bursttime;
     return chosen;
 }
